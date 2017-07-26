6c665ccef1800d219a90b313bc167858f4de18c5
Larissa Varjão <larissavarjaolsv@gmail.com>
2017-07-25 22:32:51 -0300

    Primeiro box funcionando

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,13 +1,45 @@
 var leia = document.querySelectorAll('.leia-mais');
 var tit = document.querySelector('.titulo-depoimentos');
+var carol = document.querySelector('.depoimento-carol');
+var depoimentos = document.querySelectorAll('.dep-text');
+var close = document.querySelectorAll('.close');
+
+var boxcarol = document.querySelector('#box-carol');
+var boxlari = document.querySelector('#box-lari');
+var boxclara = document.querySelector('#box-clara');
+
+
 
 Array.prototype.forEach.call(leia, function(item, index, array){
-    item.addEventList('click', function(){
-        if(index == 0)
-            tit.style.color = 'white';
-        else if (index == 1)
-            tit.style.color = 'pink';
-        else if(index == 2)
-            tit.style.color = 'blue';
+    item.addEventListener('click', function(){
+        if(index == 0){
+            boxcarol.style.display = 'none';
+            boxlari.style.display = 'none';
+            boxclara.style.display = 'none';
+            carol.style.display = 'block';
+        }
+        else if (index == 1){
+
+        }
+        else if(index == 2){
+
+        }
+    })
+})
+
+Array.prototype.forEach.call(close, function(item, index, array){
+    item.addEventListener('click', function(){
+        if(index == 0){
+            boxcarol.style.display = 'flex';
+            boxlari.style.display = 'flex';
+            boxclara.style.display = 'flex';
+            carol.style.display = 'none';
+        }
+        else if (index == 1){
+
+        }
+        else if(index == 2){
+
+        }
     })
 })
